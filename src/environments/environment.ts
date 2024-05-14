88bc1cabe3d15a73e0180ebcec908d6dd581ca3b
anushakunta <56951542+anushakunta@users.noreply.github.com>
2024-05-14 13:39:44 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: "AIzaSyASCxHgCSpmVnQwVbLCHATcns5fJQFdmpY",
     authDomain: "clips-36548.firebaseapp.com",
